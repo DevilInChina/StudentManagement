665dc3b7555bcbcce39784e3484e23bf208e0140
DevilInChina <905320627@qq.com>
2020-04-25 10:28:01 +0800

    添加项目文件。

---
+++ DropDownMenu/DropDownMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+using System.Windows;
+namespace StudentManagement.DropDownMenu
+{
+    public class ItemMenu
+    {
+        public ItemMenu(string header, List<SubItem> subItems, PackIconKind icon)
+        {
+            Header = header;
+            SubItems = subItems;
+            Icon = icon;
+        }
+
+        public string Header { get; private set; }
+        public PackIconKind Icon { get; private set; }
+        public List<SubItem> SubItems { get; private set; }
+        public UserControl Screen { get; private set; }
+    }
+    public class SubItem
+    {
+        public SubItem(string name, UserControl screen = null)
+        {
+            Name = name;
+            Screen = screen;
+        }
+        public string Name { get; private set; }
+        public UserControl Screen { get; private set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using StudentManagement.DropDownMenu;
+using MaterialDesignThemes.Wpf;
+namespace StudentManagement
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<SubItem> []SubMenu = new List<SubItem>[5];
+            for(int i = 0; i < 5; ++i)
+            {
+                SubMenu[i] = new List<SubItem>();
+                SubMenu[i].Add(new SubItem("A"));
+                SubMenu[i].Add(new SubItem("B"));
+                SubMenu[i].Add(new SubItem("C"));
+                SubMenu[i].Add(new SubItem("D"));
+                Menu.Children.Add(new UserControlMenuItem(new ItemMenu("Header "+i, SubMenu[i], PackIconKind.Schedule), this));
+            }
+        }
+
+        internal void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+
+            if (screen != null)
+            {
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+            }
+        }
+    }
+}
